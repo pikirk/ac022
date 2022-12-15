96da1dadba0e5db59f7b06e535ff39c3179d3aed
Paul Kirk <pikirk@gmail.com>
2022-12-14 18:59:24 -0800

    Feat/day6 (#6)
    * fix: day6.1 solution
    
    * fix: day6.2 solution
    
    * fix: remove path
    
    Co-authored-by: Paul Kirk <pikirk@Pauls-MacBook-Pro.local>

---
+++ day6/1/answer.py
@@ -0,0 +1,19 @@
+signal = ""
+with open('input.txt', 'r') as fp:
+    while True:
+        char = fp.read(1)
+        if not char: break
+        signal += char
+        
+packet = [signal[0], signal[1], signal[2], signal[3]]
+position = 4
+for c in range(4, len(signal)):
+    if len(packet) < 4:
+        packet.append(signal[c -1])
+    if len(set(packet)) == 4: # uniqueness check
+        break;
+    else:
+        packet.pop(0)
+    position += 1
+
+print (position)
