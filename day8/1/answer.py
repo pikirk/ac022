d9d8b42ba3ce630e7057eb2d3f987a3a4ca3b0b3
Paul Kirk <pikirk@Pauls-MacBook-Pro.local>
2023-01-10 12:20:20 -0800

    fix: corner grid state

--- day8/1/answer.py
+++ day8/1/answer.py
@@ -11,19 +11,37 @@ class Picker:
         self.bot = ()
         self.left = ()
         self.center = ()
+        self.grid_height = 0
+        self.grid_width = 0
         self.is_corner = False
         self.is_edge = False
+        self.top_edge_max = ()
+        self.top_right_edge_max = ()
+        self.bottom_edge_max = ()
+        self.bottom_right_edge_max = ()
     
     @staticmethod
-    def init() -> Picker:
-        picker = Picker()
-        picker.top = (0,1)
-        picker.right = (1,2)
-        picker.bot = (2,1)
-        picker.left = (1,0)
-        picker.center = (1,1)
-        picker.is_corner = True
-        return picker
+    def init(grid_height:int, grid_width:int) -> Picker:
+        p = Picker()
+        p.top = (0,1)
+        p.right = (1,2)
+        p.bot = (2,1)
+        p.left = (1,0)
+        p.center = (1,1)
+        p.grid_height = grid_height
+        p.grid_width = grid_width
+        p.is_corner = True
+        p.top_edge_max = (0, p.grid_width - 2)
+        p.top_right_edge_max = (1, p.grid_width - 1)
+        p.bottom_edge_max = (p.grid_height -1, p.grid_width - 2)
+        p.bottom_right_edge_max = (p.grid_height -2, p.grid_width -1)
+        p.bottom_edge_min = (p.grid_height -1, 1)
+        p.bottom_left_edge_min = (p.grid_height -2, 0)
+        return p
+
+    def visible(self) -> bool:
+        print("is visible")
+        return False
 
     @staticmethod
     def shift_right(p:Picker):
@@ -32,6 +50,7 @@ class Picker:
         p.bot = (p.bot[0], p.bot[1] + 1)
         p.left = (p.left[0], p.left[1] + 1)
         p.center = (p.center[0], p.center[1] + 1)
+        Picker.setBoundaryState(p)
 
     @staticmethod
     def start_left(p:Picker):
@@ -40,35 +59,51 @@ class Picker:
         p.bot = (p.bot[0] + 1, p.bot[1])
         p.left = (p.left[0] + 1, p.left[1])
         p.center = (p.center[0] + 1, p.center[1])
+        Picker.setBoundaryState(p)
 
     @staticmethod
-    def preview_shift_right(p:Picker, grid_width:int):
-        edge_test = p.right[1] + 1
-        return edge_test <= grid_width
+    def setBoundaryState(p:Picker):
+        p.is_corner = False
+        p.is_edge = False
+        
+        # corner states - grid intitialized to top left corner
+        # TODO edges
+        if ( p.right == p.top_right_edge_max and p.top == p.top_edge_max ):
+            p.is_corner = True
 
-    @staticmethod
-    def preview_start_left(p:Picker, grid_height:int):
-        edge_test = p.bot[0] + 1
-        return edge_test <= grid_height
+        elif ( p.right == p.bottom_right_edge_max and p.bot == p.bottom_edge_max ):
+            p.is_corner = True
 
-    @staticmethod
-    def is_hidden(p:Picker) -> bool:
-        return False
+        elif ( p.bot == p.bottom_edge_min and p.left == p.bottom_left_edge_min ):
+            p.is_corner = True
+            
+    def preview_shift_right(self):
+        edge_test = self.right[1] + 1
+        return edge_test <= self.grid_width
+
+    def preview_start_left(self):
+        edge_test = self.bot[0] + 1
+        return edge_test <= self.grid_height
 
 class Grid:
     def __init(self, tree_map:list[list[int]]):
-        self.cur_picker = Picker.init()
+        h = len(tree_map)
+        w = len(len(tree_map[0]))
+        self.cur_picker = Picker.init(h, w)
         self.map = tree_map
 
     def movePicker(self) -> Tuple(int, bool):
         ret_value = ()
-        if self.preview_shift_right:
+        can_shift_right = self.cur_picker.preview_shift_right(len(self.map))
+        can_start_left = self.cur_picker.preview_shift_right(len(self.map))
+        if can_shift_right:
             Picker.shift_right(self.cur_picker)
-            ret_value = (self.cur_picker.center, Picker.is_hidden)           
-        elif self.preview_start_left:
+            ret_value = (self.cur_picker.center, self.cur_picker.visible())           
+        elif can_start_left:
             Picker.start_left(self.cur_picker)
-            ret_value = (self.cur_picker.center, Picker.is_hidden)
+            ret_value = (self.cur_picker.center, self.cur_picker.visible())
         return ret_value
 
     def getCurrentPicker(self):
         return self.cur_picker
+
--- day8/1/test.py
+++ day8/1/test.py
@@ -6,7 +6,7 @@ from answer import Picker
 
 def test_picker_init():
     # arrange
-    p = Picker.init()
+    p = Picker.init(5,5)
 
     # assert
     assert p.top == (0,1)
@@ -17,7 +17,7 @@ def test_picker_init():
 
 def test_shift_right():
     # arrange
-    p = Picker.init()
+    p = Picker.init(5,5)
 
     # act
     Picker.shift_right(p)
@@ -31,7 +31,7 @@ def test_shift_right():
 
 def test_start_left():
     # arrange
-    p = Picker.init()
+    p = Picker.init(5,5)
 
     # act
     Picker.start_left(p)
@@ -45,7 +45,7 @@ def test_start_left():
 
 def test_start_left_then_shift_right():
     # arrange
-    p = Picker.init()
+    p = Picker.init(5,5)
 
     # act
     Picker.start_left(p)
@@ -60,48 +60,106 @@ def test_start_left_then_shift_right():
 
 def test_cannot_shift_right():
     # arrange
-    p = Picker.init()
+    p = Picker.init(5,5)
     for m in range(0, 3):
         Picker.shift_right(p)
 
     # act
-    result = Picker.preview_shift_right(p, 5)
+    result = Picker.preview_shift_right(p)
 
     # assert
     assert result == False
 
 def test_last_shift_right():
     # arrange
-    p = Picker.init()
+    p = Picker.init(5,5)
     for m in range(0, 2):
         Picker.shift_right(p)
 
     # act
-    result = Picker.preview_shift_right(p, 5)
+    result = Picker.preview_shift_right(p)
 
     # assert
     assert result == True
 
 def test_cannot_start_left():
     # arrange
-    p = Picker.init()
+    p = Picker.init(5,5)
     for m in range(0, 3):
         Picker.start_left(p)
 
     # act
-    result = Picker.preview_start_left(p, 5)
+    result = Picker.preview_start_left(p)
 
     # assert
     assert result == False
 
 def test_last_start_left():
     # arrange
-    p = Picker.init()
+    p = Picker.init(5,5)
     for m in range(0, 2):
         Picker.start_left(p)
 
     # act
-    result = Picker.preview_start_left(p, 5)
+    result = Picker.preview_start_left(p)
 
     # assert
     assert result == True
+
+def test_initial_position_is_top_left():
+    # arrange
+    p = Picker.init(5,5)
+
+    # assert
+    assert p.is_corner == True
+
+def test_position_is_top_right():
+    # arrange
+    p = Picker.init(5,5)
+
+    # act
+    for c in range(0, 2):
+        Picker.shift_right(p)
+    #Picker.setBoundaryState(p)
+
+    # assert
+    assert p.is_corner == True
+    assert p.is_edge == False
+
+def test_position_is_bottom_right():
+    # arrange
+    p = Picker.init(5,5)
+
+    # act
+    for r in range(0, 2):
+        Picker.start_left(p)
+    
+    for c in range(0, 2):
+        Picker.shift_right(p)
+
+    # assert
+    assert p.is_corner == True
+    assert p.is_edge == False
+
+def test_position_is_bottom_left():
+    # arrange
+    p = Picker.init(5,5)
+
+     # act
+    for r in range(0, 2):
+        Picker.start_left(p)
+
+     # assert
+    assert p.is_corner == True
+    assert p.is_edge == False
+
+def test_position_out_of_bounds():
+    p = Picker.init(5,5)
+
+     # act
+    for r in range(0, 3):
+        Picker.start_left(p)
+
+    # assert
+    assert p.is_corner == False
+    assert p.is_edge == False
